d03cae9e1cc5f074ba9689c71b2c7c6c4535a235
Anand Gautam <103447008+AnandGautam123@users.noreply.github.com>
2024-04-05 19:34:04 +0530

    Update LeftSider.js

--- frontend/src/pages/Home/LeftSider.js
+++ frontend/src/pages/Home/LeftSider.js
@@ -5,18 +5,18 @@ export default function LeftSider() {
     <div className="fixed left-0 bottom-0 px-10 sm:static ">
       <div className="flex flex-col items-center">
         <div className="flex flex-col gap-3 sm:flex-row">
-          <a href="https://www.linkedin.com/in/anand--gautam">
-            <i className="ri-linkedin-box-fill text-gray-400 "></i>
-          </a>
-    <a href="https://www.instagram.com/_anand_gautam___?igsh=dmJvcGlxcTUwcGVt&utm_source=qr">
-          <i className="ri-instagram-line text-gray-400 "></i>
-    </a>
-    <a href="https://github.com/AnandGautam123">
-          <i className="ri-github-fill text-gray-400 "></i>
-    </a>
-    <a href="https://anandsportfolio.pages.dev">
-          <i className="ri-profile-line text-gray-400 "></i>
-    </a>
+            <a href="https://www.linkedin.com/in/anand--gautam">
+                <i className="ri-linkedin-box-fill text-gray-400 "></i>
+            </a>
+            <a href="https://www.instagram.com/_anand_gautam___?igsh=dmJvcGlxcTUwcGVt&utm_source=qr">
+                <i className="ri-instagram-line text-gray-400 "></i>
+            </a>
+            <a href="https://github.com/AnandGautam123">
+                <i className="ri-github-fill text-gray-400 "></i>
+            </a>
+            <a href="https://anandsportfolio.pages.dev">
+                <i className="ri-profile-line text-gray-400 "></i>
+            </a>
         </div>
         <div className="w-[1px] h-32 bg-[#e3dfdf] sm:hidden"></div>
       </div>
