11c01f4000f88d2a5c1ee1d250a0729286166fa8
Anand Gautam <103447008+AnandGautam123@users.noreply.github.com>
2024-04-05 14:58:43 +0530

    Update About.js

--- frontend/src/pages/Home/About.js
+++ frontend/src/pages/Home/About.js
@@ -1,3 +1,4 @@
+// About.js
 import React from "react";
 import SectionTitle from "../../components/SectionTitle";
 import { useSelector } from "react-redux";
@@ -6,6 +7,7 @@ const About = () => {
   const { portfolioData } = useSelector((state) => state.root);
   const { about } = portfolioData || {};
   const { skills, lottieURL, description1, description2 } = about || {};
+  
   return (
     <div>
       <SectionTitle title="About" />
@@ -27,7 +29,7 @@ const About = () => {
       <div className="py-5">
         <h1 className="text-tertiary text-2xl">Skills :</h1>
         <div className="flex flex-wrap gap-10 mt-5">
-          {skills.map((skill, index) => (
+          {skills && skills.map((skill, index) => (
             <div key={index} className="border border-tertiary py-3 px-5">
               <h1 className="text-tertiary">{skill}</h1>
             </div>
