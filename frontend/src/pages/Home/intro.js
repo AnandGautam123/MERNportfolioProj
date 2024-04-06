1abee37e860d4186493b6f1514fd0646ad1a5caf
Anand Gautam <103447008+AnandGautam123@users.noreply.github.com>
2024-04-07 02:55:47 +0530

    Update intro.js

--- frontend/src/pages/Home/intro.js
+++ frontend/src/pages/Home/intro.js
@@ -2,7 +2,7 @@ import React from "react";
 import { useSelector } from "react-redux";
 function downloadResume() {
   const resumeLink =
-    "https://drive.google.com/file/d/1kKr2Hg7H8zYjfz0ot3l_MGKhtsycbxsS/view?usp=drivesdk";
+    "https://drive.google.com/file/d/1kPmbdDOIaX5X2ToV2MDOi43evFbp0BEZ/view?usp=sharing";
   window.open(resumeLink);
 }
 
